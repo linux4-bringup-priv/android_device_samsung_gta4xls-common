70f6fe181d51255d8a6667e27b4eb8550b387cc0
Tim Zimmermann <tim@linux4.de>
2024-04-22 21:13:14 +0200

    gta4xl-common: Update blobs from P610XXS4FXA1
    Change-Id: I19963c9c3d83e57fc4c50355bd9b038f7527676f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020-2022 The LineageOS Project
+# Copyright (C) 2020-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -159,7 +159,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 ENABLE_VENDOR_RIL_SERVICE := true
 
 ## Security
-VENDOR_SECURITY_PATCH := 2023-05-01
+VENDOR_SECURITY_PATCH := 2024-02-01
 
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
