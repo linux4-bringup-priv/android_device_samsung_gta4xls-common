ee4807f9a218a0611e89ba86320b6ac2fb98c47f
Tim Zimmermann <tim@linux4.de>
2024-09-01 14:51:27 +0200

    gta4xls-common: Switch to gralloc4
    Change-Id: I2a6cc6683a86de4795c92bd3e2b969213c50a8f8

--- common.mk
+++ common.mk
@@ -100,17 +100,12 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl:64 \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.allocator@4.0-service \
+    android.hardware.graphics.mapper@4.0-impl
 
 PRODUCT_PACKAGES += \
     android.hardware.composer.hwc3-service.slsi \
-    gralloc.s5e8825 \
     libion
-
-PRODUCT_PACKAGES += \
-    libGrallocWrapper
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
